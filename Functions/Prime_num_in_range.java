7b269967ae5abad450435eb54eeac8939e2b343e
Nachiket88-codespace <nachiket88.codespace@gmail.com>
2026-06-13 11:46:15 +0530

    Function Update.

--- Functions/Prime_num_in_range.java
+++ Functions/Prime_num_in_range.java
@@ -1,32 +1,39 @@
-import java.util.* ;
+import java.util.*;
 public class Prime_num_in_range {
-    
-    public static boolean Is_prime_no(int num ){
+
+
+    public static void prime_check(int num){
         boolean is_prime = true ; 
-        for(int i=2 ; i <= Math.sqrt(num) ; i++){
-            if(num % i == 0 ){
-                is_prime = false ;
-            }
+        if(num == 2){
+            System.out.println("2 is a prime number");
         }
-        return is_prime ; 
+        else if(num != 2 ){
+            for(int i=2 ; i<=Math.sqrt(num) ; i++){
+                if(num % i == 0){
+                    is_prime = false ; 
+                }
+            }
+
+            if(is_prime == true){
+            System.out.println(num);
+        }
     }
 
-        public static void prime_in_range(int range){
-            for(int j=2 ; j <= range ; j++){
-                if(Is_prime_no(j) == true){
-                    System.out.print(j);
-                }
-                System.out.print(".");
-            }
-        }
+ }
 
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in) ; 
-            System.out.print("enter the range of number : ");
-            int range_of_num = sc.nextInt() ;
-            prime_in_range(range_of_num);
-
-            sc.close();
+    public static void prime_range(int range){
+        for(int j=2 ; j<=range ; j++){
+            prime_check(j);
         }
-    
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in) ; 
+        System.out.print("enter last digit of the search = ");
+        int last_num = sc.nextInt() ; 
+
+        prime_range(last_num) ; 
+        sc.close();
+    }
 }

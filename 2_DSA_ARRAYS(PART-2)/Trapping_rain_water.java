61615bbd2e5e773303898ca7b9c453b155c857e4
Nachiket88-codespace <nachiket88.codespace@gmail.com>
2026-06-14 10:10:19 +0530

    trapping rainwater

--- 2_DSA_ARRAYS(PART-2)/Trapping_rain_water.java
+++ 2_DSA_ARRAYS(PART-2)/Trapping_rain_water.java
@@ -1,40 +1,52 @@
-// import java.util.*;
+import java.util.*;
 public class Trapping_rain_water {
 
-    public static int water_trap(int height[]){
+    public static void trap_water_level(int bar_level[]){
 
-        if (height == null || height.length <= 2) {
-            return 0;
+        int right_max[] = new int[bar_level.length] ; 
+        right_max[0] = bar_level[0] ;
+
+        for(int i=1 ; i<bar_level.length ; i++){
+            right_max[i] = Math.max(bar_level[i], right_max[i-1]) ; 
         }
 
+        for(int j=0 ; j<right_max.length ; j++){
+            System.out.print(right_max[j] + " ");
+        }
+
+        System.out.println();
+        System.out.println("======================================================");
+        System.out.println();
+
+        int left_max[] = new int[bar_level.length] ; 
+        left_max[bar_level.length-1] = bar_level[bar_level.length-1] ; 
+
+        for(int k=bar_level.length-2 ; k>=0 ; k--){
+            left_max[k] = Math.max(bar_level[k], left_max[k+1]) ; 
+        }
+
+        for(int m=0 ; m<left_max.length ; m++){
+            System.out.print(left_max[m] + " ");
+        }
+        System.out.println();
+        System.out.println("======================================================");
+        System.out.println();
+
+        int water_level[] = new int[bar_level.length] ; 
+        for(int h=0 ; h<water_level.length ; h++){
+            water_level[h] = Math.min(left_max[h], right_max[h]) ; 
+        }
+
+        int sum = 0 ;
+        for(int a=0 ; a<bar_level.length ; a++){
+            sum = sum + (water_level[a] - bar_level[a]) ; 
+        }
         
-        int left_max_boundary[] = new int[height.length] ; 
-        left_max_boundary[0] = height[0]  ;
-        for(int i=1 ; i < height.length ; i++){
-            left_max_boundary[i] = Math.max(height[i], left_max_boundary[i-1]) ; 
-        }
-        
-        int right_max_boundary[] = new int[height.length] ; 
-        right_max_boundary[height.length-1] = height[height.length-1] ; 
-        for(int j=(height.length-2) ; j >=0 ; j--){
-            right_max_boundary[j] = Math.max(height[j], right_max_boundary[j+1]);
-        }
-
-        int trapped_water = 0 ;
-        for(int k=0 ; k < height.length ; k++){
-            int water_level = Math.min(left_max_boundary[k], right_max_boundary[k]) ; 
-            trapped_water = trapped_water + (water_level - height[k]) ; 
-        }
-        return trapped_water ; 
+        System.out.println(sum);
     }
     public static void main(String[] args) {
-        int height[] = {0,1,0,2,1,0,1,3,2,1,2,1}  ;
+        int bar_level[] = {4,2,0,6,3,2,5}  ;
 
-        int answer = water_trap(height) ; 
-        System.out.println(answer);   
-
-        
-        
-
+        trap_water_level(bar_level);
     }
 }

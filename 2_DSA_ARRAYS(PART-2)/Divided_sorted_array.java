8ff074275d2406b3b40619b672fde93285aa4047
Nachiket88-codespace <nachiket88.codespace@gmail.com>
2026-06-15 23:55:32 +0530

    Day_3 update.

--- 2_DSA_ARRAYS(PART-2)/Divided_sorted_array.java
+++ 2_DSA_ARRAYS(PART-2)/Divided_sorted_array.java
@@ -1,30 +1,30 @@
 public class Divided_sorted_array {
 
-    public static int binary_divided_search(int num_set[] , int key){
+    public static int new_binary_divided_search(int num_set[] , int key){
         int start = 0 ;
         int end = num_set.length-1 ; 
 
-        while(start<=end){
-            int mid = start + (end-start)/2 ; 
+        while (start<=end) {
+            int mid = start + (end-start)/2  ;
 
-            if(num_set[mid] == key){
-                System.out.println("key found at index = "+ mid);
-                return mid ;
+            if(key == num_set[mid]){
+                System.out.print("key found at index: "+mid);
+                return mid ; 
             }
-            else if(num_set[start] <= num_set[mid]){
-                if(key >=num_set[start] && key < num_set[mid]){
-                    end = mid -1 ;
+            else if(num_set[start]<=num_set[mid]){
+                if(key>=num_set[start] && key<num_set[mid]){
+                    end = mid-1 ; 
                 }
                 else{
-                    start = mid +1 ; 
+                    start = mid+1 ; 
                 }
             }
             else{
-                if(key > num_set[mid] && key <= num_set[end]){
-                    start = mid + 1;
+                if(key>num_set[mid] && key<=num_set[end]){
+                    start = mid+1 ; 
                 }
                 else{
-                    end = mid - 1;
+                    end = mid-1 ;
                 }
             }
         }
@@ -34,7 +34,7 @@ public class Divided_sorted_array {
         int num_set[] = {4,5,6,7,0,1,2} ; 
         int key = 0 ;
 
-        binary_divided_search(num_set, key) ; 
+        new_binary_divided_search(num_set, key) ; 
 
     }
 }

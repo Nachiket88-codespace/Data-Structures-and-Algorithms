a31d517dec5f831344b96520f6ee66c3721e72a3
Nachiket88-codespace <nachiket88.codespace@gmail.com>
2026-07-01 08:51:14 +0530

    loops que done

--- Loops/PRIME_NUM_TWO.java
+++ Loops/PRIME_NUM_TWO.java
@@ -1,31 +1,32 @@
-import java.util.*;
+import java.util.* ; 
 public class PRIME_NUM_TWO {
 
-    public static void main(String[] args) {
-         Scanner sc = new Scanner(System.in) ;
-         System.out.print("enter a number greater >= 2 : ");
-         int num = sc.nextInt() ; 
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in) ; 
+      System.out.print("enter any number = ");
+      int num = sc.nextInt();
 
-         boolean is_prime = true ; 
-         if(num == 2){
-            System.out.println("2 is a prime number ");
-         }
-         else if(num != 2){
-            for(int i=2 ; i<=Math.sqrt(num) ; i++){
-                if(num % i == 0){
-                    is_prime  =false ; 
-                }
+      boolean is_prime = true ; 
+      if(num == 2){
+         System.out.println(num + " is a prime number ");
+      }
+      else if(num>2){
+         for(int i=2 ; i<=Math.sqrt(num) ; i++){
+            if(num % i == 0){
+               is_prime = false ; 
             }
          }
+      }
 
-         if(is_prime == true){
-            System.out.print(num + " is a prime number");
-         }
-         else{
-            System.out.print(num + " is not a prime number");
-         }
+      if(is_prime == true){
+         System.out.println("num is a prime num");
+      }
+      else{
+         System.out.println("num is not a prime num");
+      }
 
-         sc.close();
-    }
+
+      sc.close();
+   }
 }
 

7fcff73025ca78b43f2b434a9b3fedf38eac3de3
Nachiket88-codespace <nachiket88.codespace@gmail.com>
2026-07-01 08:34:50 +0530

    1st 1_july push

--- Loops/PALLINDROME_NUM.java
+++ Loops/PALLINDROME_NUM.java
@@ -1,22 +1,25 @@
-import java.util.* ; 
+import java.util.*;
 public class PALLINDROME_NUM {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in) ; 
-
-        System.out.print("enter a number :");
         int num = sc.nextInt() ; 
-        int temp_num = num ; 
-        int rev_num = 0 ; 
+        int temp = num ;
+        int rev_num = 0 ;
 
-        while(temp_num > 0){
-            int last_digit = temp_num % 10 ;
-            temp_num = temp_num / 10 ;
-            rev_num = rev_num*10 + last_digit ; 
+        while(temp>0){
+            int remainder = temp % 10 ;
+            temp = temp / 10 ;
+            rev_num = rev_num*10 + remainder;
         }
 
-        if(rev_num == num){
-            System.out.println(num +" is a pallindrome number.");
+        if(num == rev_num){
+            System.out.println(num + " is a palindrome number");
         }
+        else{
+            System.out.println(num + " is not a palindrome number");
+        }
+
         sc.close();
     }
 }

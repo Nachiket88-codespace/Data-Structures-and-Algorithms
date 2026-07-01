f537b671e1e4332219a93c7dad498c5895e6da1d
Nachiket88-codespace <nachiket88.codespace@gmail.com>
2026-07-01 09:55:14 +0530

    more patterns done

---
+++ patterns/d_triangle_pattern.java
@@ -0,0 +1,23 @@
+import java.util.* ; 
+public class d_triangle_pattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter num of rows = ");
+        int total_rows = sc.nextInt() ; 
+        
+        for(int i=1 ; i<=total_rows ; i++){
+            //spaces:
+            for(int j=1 ; j<=(i-1) ; j++){
+                System.out.print(" "+" ");
+            }
+            //stars:
+            for(int j=1 ; j<=(total_rows-i+1) ; j++){
+                System.out.print("*"+" ");
+            }
+
+            System.out.println();
+        }
+        sc.close();
+    }
+    
+}

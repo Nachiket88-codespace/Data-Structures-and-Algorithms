9326cc1c23fd97a592b4236bc86775a3c9c8c330
Nachiket88-codespace <nachiket88.codespace@gmail.com>
2026-07-16 16:06:52 +0530

    triangle pattern

--- patterns/a_triangle_pattern.java
+++ patterns/a_triangle_pattern.java
@@ -1,13 +1,17 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class a_triangle_pattern {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in) ; 
-        System.out.print("enter num of rows = ");
-        int total_rows = sc.nextInt() ; 
+        
+        System.out.print("enter total num of rows = ");
+        int total_rows = sc.nextInt() ;
 
         for(int i=1 ; i<=total_rows ; i++){
+            for(int j=1 ; j<=(total_rows-i) ; j++){
+                System.out.print(" "+" ");
+            }
             for(int j=1 ; j<=i ; j++){
                 System.out.print("*"+" ");
             }

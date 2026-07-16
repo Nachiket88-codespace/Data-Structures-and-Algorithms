f468700ab4b8bc2e054ae4466099a44d7479cd14
Nachiket88-codespace <nachiket88.codespace@gmail.com>
2026-07-16 16:31:13 +0530

    more patterns done

--- patterns/hollow_rectangle.java
+++ patterns/hollow_rectangle.java
@@ -4,14 +4,14 @@ public class hollow_rectangle {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in) ; 
-        System.out.print("enter total rows = ");
-        int total_rows = sc.nextInt() ; 
-        System.out.print("enter total columns = ");
-        int total_columns = sc.nextInt() ; 
+        System.out.print("enter num, of rows = ");
+        int total_rows = sc.nextInt() ;
+        System.out.print("enter num of columns = ");
+        int total_columns = sc.nextInt() ;
 
         for(int i=1 ; i<=total_rows ; i++){
             for(int j=1 ; j<=total_columns ; j++){
-                if(i==1 || i==total_rows || j==1 || j==total_columns){
+                if(i==1 || i==total_rows  || j==1 || j==total_columns){
                     System.out.print("*"+" ");
                 }
                 else{
@@ -20,6 +20,7 @@ public class hollow_rectangle {
             }
             System.out.println();
         }
+
         sc.close();
     }
 }

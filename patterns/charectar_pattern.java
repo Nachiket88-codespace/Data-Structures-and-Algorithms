ad7218c0232713ddb4b11773d2eb0f4a9b224f61
Nachiket88-codespace <nachiket88.codespace@gmail.com>
2026-07-16 16:20:08 +0530

    char and floyds pattern done

--- patterns/charectar_pattern.java
+++ patterns/charectar_pattern.java
@@ -7,15 +7,15 @@ public class charectar_pattern {
         System.out.print("enter number of rows = ");
         int total_rows = sc.nextInt() ; 
 
-        char ch = 'A' ; 
+        char ch ='A' ;
         for(int i=1 ; i<=total_rows ; i++){
             for(int j=1 ; j<=i ; j++){
                 System.out.print(ch+" ");
-                ch++ ; 
+                ch++ ;
             }
             System.out.println();
         }
-
+        
         sc.close();
     }
 }

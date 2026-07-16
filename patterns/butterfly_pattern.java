462339f11acf8344030dee85eb457b07b369840e
Nachiket88-codespace <nachiket88.codespace@gmail.com>
2026-07-16 16:40:29 +0530

    butterfly pattern

--- patterns/butterfly_pattern.java
+++ patterns/butterfly_pattern.java
@@ -1,45 +1,46 @@
-import java.util.Scanner;
-
+import java.util.*  ;
 public class butterfly_pattern {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in) ; 
-        System.out.print("enter number of rows = ");
-        int total_rows = sc.nextInt() ; 
-        // upper part :
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter num of rows = ");
+        int total_rows = sc.nextInt() ;
+
+        //upper part:
         for(int i=1 ; i<=total_rows ; i++){
-            // stars:
+            //stars:
             for(int j=1 ; j<=i ; j++){
                 System.out.print("*"+" ");
             }
-            // spaces:
-            for(int j=1 ; j<=(total_rows-i)*2 ; j++){
+            //spaces:
+            for(int j=1 ; j<=(total_rows*2)-(i*2) ; j++){
                 System.out.print(" "+" ");
             }
-            // stars:
+            //stars:
             for(int j=1 ; j<=i ; j++){
                 System.out.print("*"+" ");
             }
 
             System.out.println();
         }
-        // lower part:
+        //lower triangle:
         for(int i=total_rows ; i>=1 ; i--){
-          // stars:
+            //stars:
             for(int j=1 ; j<=i ; j++){
                 System.out.print("*"+" ");
             }
-            // spaces:
-            for(int j=1 ; j<=(total_rows-i)*2 ; j++){
+            //spaces:
+            for(int j=1 ; j<=(total_rows*2)-(i*2) ; j++){
                 System.out.print(" "+" ");
             }
-            // stars:
+            //stars:
             for(int j=1 ; j<=i ; j++){
                 System.out.print("*"+" ");
             }
 
-            System.out.println();  
+            System.out.println();
         }
+
         sc.close();
     }
 }

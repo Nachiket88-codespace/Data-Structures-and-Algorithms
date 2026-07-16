d410801d03ad7242e8a97423706cb39bd63a5ab5
Nachiket88-codespace <nachiket88.codespace@gmail.com>
2026-07-16 16:17:44 +0530

    all triangle patterns done

--- patterns/c_triangle_pattern.java
+++ patterns/c_triangle_pattern.java
@@ -1,18 +1,19 @@
-import java.util.Scanner;
-
+import java.util.* ;
 public class c_triangle_pattern {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in) ; 
-        System.out.print("enter num of rows = ");
+
+        System.out.print("enter total num of rows = ");
         int total_rows = sc.nextInt() ; 
 
-        for(int k=1 ; k<=total_rows ; k++){
-            for(int j=1 ; j<=(total_rows-k+1) ; j++){
+        for(int i=1 ; i<=total_rows ; i++){
+            for(int j=1 ; j<=(total_rows-i+1) ; j++){
                 System.out.print("*"+" ");
             }
             System.out.println();
         }
+
         sc.close();
     }
 }
--- patterns/d_triangle_pattern.java
+++ patterns/d_triangle_pattern.java
@@ -1,24 +1,21 @@
-import java.util.Scanner;
-
+import java.util.* ;
 public class d_triangle_pattern {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in) ; 
-        System.out.print("enter num of rows = ");
+        Scanner sc = new Scanner(System.in) ;
+        System.out.print("enter total num of rows = ");
         int total_rows = sc.nextInt() ; 
 
         for(int i=1 ; i<=total_rows ; i++){
-            //space :
             for(int j=1 ; j<=(i-1) ; j++){
                 System.out.print(" "+" ");
             }
-            //stars :
             for(int j=1 ; j<=(total_rows-i+1) ; j++){
                 System.out.print("*"+" ");
             }
-
             System.out.println();
         }
+
         sc.close();
     }
 }
